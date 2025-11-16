7d8ee185b6868e759cde1ac6e921987bf9aea416
Anish Patel <jagupatel@Anishs-Macbook-Pro.local>
2025-11-16 16:59:32 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,37 @@
+# https://github.com/anishpat26-commits/lab10-AP-JJ.git
+# Partner 1: Anish Patel
+# Partner 2: James Jiang
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Logarithm base must be not be 1 or less than or equal to zero..")
+    if b <= 0:
+        raise ValueError("Logarithm argument must be greater than zero.")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
+
 
 
